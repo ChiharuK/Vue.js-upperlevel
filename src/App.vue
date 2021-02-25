b60ca23da7cff7057c7abd584f8be8ff53574221
Chiharu <thousands.spring43@gmail.com>
2021-02-25 17:37:39 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="zip" />
+    <button @click="askAddress">住所自動入力</button>
+    <p>Address:{{ allAddress }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      zip:"",
+      allAddress:"",
+    };
+  },
+  methods: {
+    askAddress() {
+      axios.get("https://apis.postcode-jp.com/api/v4/postcodes/string?apiKey=string=	U4SDshjc0nvYZTLSI20FCNv6Mk7yB3uRXHXNCT7").then((response) => {this.allAddress = response.data.results[0];})
+    },
+  }
+}
+</script>
